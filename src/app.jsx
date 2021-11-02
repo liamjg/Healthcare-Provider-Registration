76d753107f82bfd13321195739dfb4be518fe87b
Liam Gallagher <liam@ibm.com>
2021-11-01 23:02:03 -0500

    extracted form logic to custom hook

--- src/app.jsx
+++ src/app.jsx
@@ -1,91 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+import useForm from './form-hook';
+
 import './app.scss';
 
-import {
-  validateNPINum,
-  validateEmailAddr,
-  validateTelephoneNumber,
-} from './validators';
-
-const initialFormData = {
-  firstName: '',
-  lastName: '',
-  npiNum: '',
-  teleNum: '',
-  bizAddress: '',
-  email: '',
-};
-
-const initialFormErrors = {
-  npiNum: false,
-  teleNum: false,
-  email: false,
-};
-
 const App = () => {
-  const [formData, setFormData] = useState(initialFormData);
-  const [formErrors, setFormErrors] = useState(initialFormErrors);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-
-    switch (name) {
-      case 'firstName':
-        setFormData({ ...formData, firstName: value });
-        break;
-
-      case 'lastName':
-        setFormData({ ...formData, lastName: value });
-        break;
-
-      case 'npiNum':
-        if (!value || validateNPINum(value)) {
-          setFormData({ ...formData, npiNum: value });
-          setFormErrors({ ...formErrors, npiNum: false });
-        } else {
-          setFormData({ ...formData, npiNum: '' });
-          setFormErrors({ ...formErrors, npiNum: true });
-        }
-        break;
-
-      case 'teleNum':
-        if (!value || validateTelephoneNumber(value)) {
-          setFormData({ ...formData, teleNum: value });
-          setFormErrors({ ...formErrors, teleNum: false });
-        } else {
-          setFormData({ ...formData, teleNum: '' });
-          setFormErrors({ ...formErrors, teleNum: true });
-        }
-        break;
-
-      case 'bizAddress':
-        setFormData({ ...formData, bizAddress: value });
-        break;
-
-      case 'email':
-        if (!value || validateEmailAddr(value)) {
-          setFormData({ ...formData, email: value });
-          setFormErrors({ ...formErrors, email: false });
-        } else {
-          setFormData({ ...formData, email: '' });
-          setFormErrors({ ...formErrors, email: true });
-        }
-        break;
-
-      default:
-        break;
-    }
-  };
+  const [formData, formErrors, handleFormEvent, resetForm] = useForm();
 
   const handleSubmit = (e) => {
-    // this depends heavily on what we want to do with the data / form when finished
+    // this function depends heavily on what we want to do with the data / form when finished
     e.preventDefault();
 
     console.log(formData);
 
     document.getElementById('healthcare-registration-form').reset();
-    setFormData(initialFormData);
-    setFormErrors(initialFormErrors);
+    resetForm();
   };
 
   const filled = Object.values(formData).every((value) => value !== '');
@@ -110,7 +39,7 @@ const App = () => {
                 placeholder="First Name"
                 type="text"
                 name="firstName"
-                onBlur={handleChange}
+                onBlur={handleFormEvent}
                 noValidate
               />
             </span>
@@ -121,7 +50,7 @@ const App = () => {
                 placeholder="Last Name"
                 type="text"
                 name="lastName"
-                onBlur={handleChange}
+                onBlur={handleFormEvent}
                 noValidate
               />
             </span>
@@ -134,7 +63,7 @@ const App = () => {
                 placeholder="NPI number"
                 type="text"
                 name="npiNum"
-                onBlur={handleChange}
+                onBlur={handleFormEvent}
                 noValidate
               />
               {formErrors.npiNum && <small>Enter a valid NPI number</small>}
@@ -148,7 +77,7 @@ const App = () => {
                 placeholder="Business address"
                 type="text"
                 name="bizAddress"
-                onBlur={handleChange}
+                onBlur={handleFormEvent}
                 noValidate
               />
             </span>
@@ -161,7 +90,7 @@ const App = () => {
                 placeholder="Telephone number"
                 type="text"
                 name="teleNum"
-                onBlur={handleChange}
+                onBlur={handleFormEvent}
                 noValidate
               />
               {formErrors.teleNum && (
@@ -177,7 +106,7 @@ const App = () => {
                 placeholder="Email address"
                 type="text"
                 name="email"
-                onBlur={handleChange}
+                onBlur={handleFormEvent}
                 noValidate
               />
               {formErrors.email && <small>Enter a valid email address</small>}
---
+++ src/form-hook.jsx
@@ -0,0 +1,100 @@
+import { useState } from 'react';
+
+// validates North American phone numbers
+const PHONE_REGEX = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+// source: https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s02.html
+
+// validates email addresses
+const EMAIL_REGEX =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// source: https://emailregex.com/
+
+// validates NPI length and composition (but not checksum)
+const NPI_REGEX = /^\d{10}$/;
+// source: me + wikipedia
+
+const initialFormData = {
+  firstName: '',
+  lastName: '',
+  npiNum: '',
+  teleNum: '',
+  bizAddress: '',
+  email: '',
+};
+
+const initialFormErrors = {
+  npiNum: false,
+  teleNum: false,
+  email: false,
+};
+
+const validateTelephoneNumber = (teleNum) => PHONE_REGEX.test(teleNum);
+
+const validateEmailAddr = (emailAddr) => EMAIL_REGEX.test(emailAddr);
+
+const validateNPINum = (npiNum) => NPI_REGEX.test(npiNum);
+
+const useForm = () => {
+  const [formData, setFormData] = useState(initialFormData);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+
+  const resetForm = () => {
+    setFormData(initialFormData);
+    setFormErrors(initialFormErrors);
+  };
+
+  const handleFormEvent = (e) => {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case 'firstName':
+        setFormData({ ...formData, firstName: value });
+        break;
+
+      case 'lastName':
+        setFormData({ ...formData, lastName: value });
+        break;
+
+      case 'npiNum':
+        if (!value || validateNPINum(value)) {
+          setFormData({ ...formData, npiNum: value });
+          setFormErrors({ ...formErrors, npiNum: false });
+        } else {
+          setFormData({ ...formData, npiNum: '' });
+          setFormErrors({ ...formErrors, npiNum: true });
+        }
+        break;
+
+      case 'teleNum':
+        if (!value || validateTelephoneNumber(value)) {
+          setFormData({ ...formData, teleNum: value });
+          setFormErrors({ ...formErrors, teleNum: false });
+        } else {
+          setFormData({ ...formData, teleNum: '' });
+          setFormErrors({ ...formErrors, teleNum: true });
+        }
+        break;
+
+      case 'bizAddress':
+        setFormData({ ...formData, bizAddress: value });
+        break;
+
+      case 'email':
+        if (!value || validateEmailAddr(value)) {
+          setFormData({ ...formData, email: value });
+          setFormErrors({ ...formErrors, email: false });
+        } else {
+          setFormData({ ...formData, email: '' });
+          setFormErrors({ ...formErrors, email: true });
+        }
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  return [formData, formErrors, handleFormEvent, resetForm];
+};
+
+export default useForm;
